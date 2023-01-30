90745fc25c9f6302afd5893961182007abe923a0
SimonStoneman <116112151+SimonStoneman@users.noreply.github.com>
2023-01-30 15:08:35 +0000

    Created two other create functions for Engineer & Inter is a similar vien to the manager one. Created a func for chooseing the additional members under the manager and then call the appropriate funcs e.g. 'add engineer' call createEnginee. Added another .then catch on the createEngineer/Intern to recall chooseAdditionalMembers func

--- index.js
+++ index.js
@@ -39,32 +39,161 @@ const daz = new Intern('Daz', 3, 'daz@test.com', 'Nextdoor');
 
 function createManager() {
     inquirer.prompt([
-        {
+        {   
+            type: 'input',
             name: 'name',
             message: 'Please enter your Name:'
         },
         {
+            type: 'input',
             name: 'id',
             message: 'Please enter your ID:'
         },
         {
+            type: 'input',
             name: 'email',
             message: 'Please enter your Email (e.g. x@y.com):'
         },
         {
+            type: 'input',
             name: 'officenumber',
             message: 'Please enter your office number:'
         },
 
     ]).then(data => {
-        teamMembersArr.push(data);
-        console.log(`teamMembersArr has: ${teamMembersArr}`);
-    });
+        // teamMembersArr.push(data);
+        const name = data.name;
+        const id = data.id;
+        const email = data.email;
+        const officenumber = data.officenumber;
+
+        console.log(data.name)
+        console.log(data.id)
+        console.log(data.email)
+        console.log(data.officenumber)
+
+        const manager = new Manager(name, id, email, officenumber);
+
+        teamMembersArr.push(manager);
+        console.log(`teamMembersArr has: ${teamMembersArr[0].name}`);
+        
+    }).then(chooseAdditionalMembers());
 }
+
+function createEngineer() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please enter your Name:'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter your ID:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your Email (e.g. x@y.com):'
+        },
+        {
+            type: 'input',
+            name: 'githubname',
+            message: 'Please enter your Github Name:'
+        },
+
+    ]).then(data => {
+        // teamMembersArr.push(data);
+        const name = data.name;
+        const id = data.id;
+        const email = data.email;
+        const githubname = data.githubname;
+
+        console.log(data.name)
+        console.log(data.id)
+        console.log(data.email)
+        console.log(data.githubname)
+
+        const engineer = new Engineer(name, id, email, githubname);
+
+        teamMembersArr.push(engineer);
+        console.log(`teamMembersArr has: ${teamMembersArr[0].name}`);
+
+    }).then(chooseAdditionalMembers);
+}
+
+function createIntern() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please enter your Name:'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter your ID:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your Email (e.g. x@y.com):'
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Please enter your School Name:'
+        },
+
+    ]).then(data => {
+        // teamMembersArr.push(data);
+        const name = data.name;
+        const id = data.id;
+        const email = data.email;
+        const school = data.school;
+
+        console.log(data.name)
+        console.log(data.id)
+        console.log(data.email)
+        console.log(data.school)
+
+        const intern = new Intern(name, id, email, school);
+
+        teamMembersArr.push(intern);
+        console.log(`teamMembersArr has: ${teamMembersArr[0].name}`);
+
+    }).then(chooseAdditionalMembers);
+}
+
+function finaliseForHtml() {
+    console.log('---------EXITING---------');
+}
+
+function chooseAdditionalMembers() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'members',
+            message: 'Add additional team members:',
+            choices: ['Add an engineer', 'Add an intern', 'Finish building the team']
+        }
+    ]).then(answer => {
+        switch(answer.choices) {
+            case 'Add an engineer':
+                createEngineer();
+            case 'Add an intern':
+                createIntern();
+            case 'Finish building the team':
+                finaliseForHtml();
+        };
+    });
+};
+
 
 function init() {
     createManager();
-}
+};
 
 init();
 
