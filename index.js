45c91a6dfd9b23df2a6afd46a6899f52e06ebeeb
SimonStoneman <116112151+SimonStoneman@users.noreply.github.com>
2023-01-30 12:02:59 +0000

    corrected maanger,engineer & intern to extend the employee class after later class learning and not trying to use prototyping:).Added creatManager func to index to ask for info and look to create a new manager class obj with prompt data, then add to teamMemberArr. Created init func to call sub functions. program initalised with call to init.

--- index.js
+++ index.js
@@ -10,10 +10,64 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-const newManager = Manager;
+const teamMembersArr = [];
 
-console.log(newManager);
-// console.log(newManager.getRole());
+const dave = new Manager('Dave', 1, 'x@y.com', 1);
+const bob = new Engineer('Bob', 2, 'bob@test.com', 'Bobby');
+const daz = new Intern('Daz', 3, 'daz@test.com', 'Nextdoor');
+
+// console.log('Manager');
+// dave.getId();
+// dave.getName();
+// dave.getEmail();
+// dave.getRole();
+// dave.getOfficeNumber();
+
+// console.log('Engineer');
+// bob.getId();
+// bob.getName();
+// bob.getEmail();
+// bob.getRole();
+// bob.getGithub();
+
+// console.log('Intern');
+// daz.getId();
+// daz.getName();
+// daz.getEmail();
+// daz.getRole();
+// daz.getSchool();
+
+function createManager() {
+    inquirer.prompt([
+        {
+            name: 'name',
+            message: 'Please enter your Name:'
+        },
+        {
+            name: 'id',
+            message: 'Please enter your ID:'
+        },
+        {
+            name: 'email',
+            message: 'Please enter your Email (e.g. x@y.com):'
+        },
+        {
+            name: 'officenumber',
+            message: 'Please enter your office number:'
+        },
+
+    ]).then(data => {
+        teamMembersArr.push(data);
+        console.log(`teamMembersArr has: ${teamMembersArr}`);
+    });
+}
+
+function init() {
+    createManager();
+}
+
+init();
+
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
